774255cbc88af357d6be1755c2441827221d61ef
Cleiton Nunes <cleiton.rodrigues6102@gmail.com>
2020-07-31 16:00:18 -0300

    chore: add JUnit

--- src/app/Controller.java
+++ src/app/Controller.java
@@ -1,10 +1,11 @@
-package sample;
+package app;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import javafx.scene.control.*;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.HttpClient;
@@ -14,7 +15,6 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.InetAddress;
 
 
 public class Controller {
@@ -34,7 +34,7 @@ public class Controller {
 
 	private Alert alert = new Alert(Alert.AlertType.ERROR);
 
-	public void apiRequest(String city) throws IOException {
+	public void apiRequest(String city) {
 		piLoading.setVisible(true);
 		String uri = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&units=metric&APPID=8b3538109fbb0da4554d4d6cf409d6e8";
 		HttpClient client = new DefaultHttpClient();
@@ -55,7 +55,6 @@ public class Controller {
 
 				throw new IOException();
 			}
-
 
 			BufferedReader rd = new BufferedReader
 				(new InputStreamReader(
@@ -89,10 +88,16 @@ public class Controller {
 	}
 
 	private String formatTextInput(String text) {
-		return text.replaceAll(" ", "+").replaceFirst(",", "%2C");
+		StringUtils stringUtils = new StringUtils();
+
+		text = stringUtils.RemoveSpecialCharacter(text);
+		text = stringUtils.convertSpaces(text);
+		text = stringUtils.replaceComma(text);
+
+		return text;
 	}
 
-	public void handleSearch() throws IOException {
+	public void handleSearch() {
 		piLoading.setProgress(-1);
 
 		String text = tfCity.getText();
@@ -108,5 +113,9 @@ public class Controller {
 		String formattedText = formatTextInput(text);
 
 		this.apiRequest(formattedText);
+	}
+
+	public void handleKeyPress(KeyEvent e) {
+		if(e.getCode() == KeyCode.ENTER) this.handleSearch();
 	}
 }
